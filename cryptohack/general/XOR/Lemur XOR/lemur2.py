6e977f49b8fc4a91a955d6f6e04f67c3d9e76c73
Always Trying To Be Better <103570217+FTN-Dev@users.noreply.github.com>
2025-06-01 14:03:56 +0700

    lemur

--- cryptohack/general/XOR/Lemur XOR/lemur2.py
+++ cryptohack/general/XOR/Lemur XOR/lemur2.py
@@ -1,15 +1,37 @@
-# rumus buatan sendiri wok.
 from PIL import Image, ImageChops
+im1 = Image.open('lemur_ed66878c338e662d3473f0d98eedbd0d.png')
+im2 = Image.open('flag_7ae18c704272532658c10b5faad06d74.png')
 
-def get_xor(image_1, image_2):
+im3 = ImageChops.add(ImageChops.subtract(im2, im1), ImageChops.subtract(im1, im2))
+im3.show()
+im3.save("./flagsssss.png")
 
-    i1 = ImageChops.invert(image_1)
-    i2 = ImageChops.invert(image_2)
 
-    return ImageChops.invert(ImageChops.add(ImageChops.subtract(i2, i1), ImageChops.subtract(i1, i2)))
 
-im2 = Image.open('img/flag_7ae18c704272532658c10b5faad06d74.png')
-im1 = Image.open('img/lemur_ed66878c338e662d3473f0d98eedbd0d.png')
 
-im3 = get_xor(im1, im2)
-im3.save("imageflag.png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
